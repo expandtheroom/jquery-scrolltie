af6bd47105ef29898ceffbd5e6bd0cc90c6f4101
Megan Tong <meganhotong@gmail.com>
2015-01-27 14:26:28 -0500

    calculate static transform on first request instead of initialization

--- dist/scrollTie.js
+++ dist/scrollTie.js
@@ -529,7 +529,6 @@ function TransformPropertyUpdater(element, opts) {
     
     PropertyUpdater.call(this, element, opts);
     
-    this.staticTransformValue = this._getStaticTransformValue();
 }
 
 extend(PropertyUpdater, TransformPropertyUpdater, {
@@ -569,6 +568,8 @@ extend(PropertyUpdater, TransformPropertyUpdater, {
 
     _formatJqueryCssVal: function(moveValue) {
         moveValue = this.propertyValueFormat((this.transform == 'scale') ? moveValue : Math.floor(moveValue));
+        
+        this.staticTransformValue = this.staticTransformValue || this._getStaticTransformValue();
 
         var transformValueWithPrefixes = {
             transform: this.staticTransformValue + ' ' + moveValue
